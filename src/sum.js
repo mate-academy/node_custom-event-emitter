0487188a6f935bd76603275bf22e47251908f930
Misha Grinko <glukym@gmail.com>
2022-06-21 15:06:45 +0300

    Update sum.js

--- src/sum.js
+++ src/sum.js
@@ -1,21 +1,12 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+export class MyEventEmitter {
+  on() {}
+  once() {}
+  off() {}
+  emit() {}
+  prependListener() {}
+  prependOnceListener() {}
+  removeAllListeners() {}
+  listenerCount() {}
 }
-
-module.exports = sum;
