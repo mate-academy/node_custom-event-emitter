6fa74bb3ea0d3555f4dd8c3e3b41652ed7670ae0
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-16 14:30:07 +0300

    Added task solution code

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,160 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this._handlers = {};
+  }
+
+  on(eventName, handler) {
+    this._checkEventName(eventName);
+    this._checkEventHandler(handler);
+
+    if (!this._handlers.hasOwnProperty(eventName)) {
+      this._handlers[eventName] = [];
+    }
+
+    this._handlers[eventName].push({
+      handler,
+      permanent: true,
+    });
+
+    return this;
+  }
+
+  once(eventName, handler) {
+    this._checkEventName(eventName);
+    this._checkEventHandler(handler);
+
+    if (!this._handlers.hasOwnProperty(eventName)) {
+      this._handlers[eventName] = [];
+    }
+
+    this._handlers[eventName].push({
+      handler,
+      permanent: false,
+    });
+
+    return this;
+  }
+
+  off(eventName, handler) {
+    this._checkEventName(eventName);
+    this._checkEventHandler(handler);
+
+    if (!this._handlers.hasOwnProperty(eventName)) {
+      return this;
+    }
+
+    this._handlers[eventName] = this._handlers[eventName].filter(
+      eventObject => eventObject.handler !== handler,
+    );
+
+    if (!this._handlers[eventName].length) {
+      delete this._handlers[eventName];
+    }
+
+    return this;
+  }
+
+  emit(eventName, ...args) {
+    this._checkEventName(eventName);
+
+    if (!this._handlers.hasOwnProperty(eventName)) {
+      return false;
+    }
+
+    const eventsToEmit = [...this._handlers[eventName]];
+
+    eventsToEmit.forEach(eventObject => {
+      eventObject.handler(...args);
+
+      if (!eventObject.permanent) {
+        this._handlers[eventName] = this._handlers[eventName].filter(
+          evObject => evObject.handler !== eventObject.handler,
+        );
+      }
+    });
+
+    return true;
+  }
+
+  prependListener(eventName, handler) {
+    this._checkEventName(eventName);
+    this._checkEventHandler(handler);
+
+    if (!this._handlers.hasOwnProperty(eventName)) {
+      this._handlers[eventName] = [];
+    }
+
+    this._handlers[eventName].unshift({
+      handler,
+      permanent: true,
+    });
+
+    return this;
+  }
+
+  prependOnceListener(eventName, handler) {
+    this._checkEventName(eventName);
+    this._checkEventHandler(handler);
+
+    if (!this._handlers.hasOwnProperty(eventName)) {
+      this._handlers[eventName] = [];
+    }
+
+    this._handlers[eventName].unshift({
+      handler,
+      permanent: false,
+    });
+
+    return this;
+  }
+
+  removeAllListeners(eventName) {
+    if (!eventName) {
+      this._handlers = [];
+
+      return this;
+    }
+
+    this._checkEventName(eventName);
+
+    if (!this._handlers.hasOwnProperty(eventName)) {
+      return this;
+    }
+
+    delete this._handlers[eventName];
+
+    return this;
+  }
+
+  listenerCount(eventName) {
+    if (!this._handlers.hasOwnProperty(eventName)) {
+      return 0;
+    }
+
+    return this._handlers[eventName].length;
+  }
+
+  _checkEventName(eventName) {
+    if (!eventName) {
+      throw new Error('Event name must be defined!');
+    }
+
+    if (typeof eventName !== 'string') {
+      throw new Error('Event name must be a string!');
+    }
+  }
+
+  _checkEventHandler(handler) {
+    if (!handler) {
+      throw new Error('Event handler must be defined!');
+    }
+
+    if (typeof handler !== 'function') {
+      throw new Error('Event handler must be a function!');
+    }
+  }
 }
+
+module.exports = { MyEventEmitter };
