b09ea9e3721416fcbd9467bed0eea39fddddb4f0
Max Bovtun <maxbovtun@gmail.com>
2022-10-30 14:20:58 +0200

    solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,97 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [callback];
+    } else {
+      this.events[eventName].push(callback);
+    }
+  }
+
+  once(eventName, callback) {
+    this.listeners[eventName] = this.listeners[eventName] || [];
+
+    const onceWrapper = () => {
+      callback();
+      this.off(eventName, onceWrapper);
+    };
+
+    this.listeners[eventName].push(onceWrapper);
+  }
+
+  off(eventName, callback) {
+    this.events[eventName] = this.events[eventName] || [];
+
+    for (let i = 0; i < this.events[eventName].length; i++) {
+      if (this.events[eventName] === callback) {
+        this.events[eventName].splice(i, 1);
+
+        return;
+      }
+    }
+  }
+
+  emit(eventName, ...args) {
+    if (!this.events[eventName]) {
+      return;
+    }
+
+    this.events[eventName].forEach(callback => {
+      callback(...args);
+    });
+  }
+
+  prependListener(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [callback];
+    } else {
+      this.events[eventName].unshift(callback);
+    }
+  }
+
+  prependOnceListener(eventName, callback) {
+    this.listeners[eventName] = this.listeners[eventName] || [];
+
+    const onceWrapper = () => {
+      callback();
+      this.off(eventName, onceWrapper);
+    };
+
+    this.listeners[eventName].unshift(onceWrapper);
+  }
+
+  removeAllListeners(eventName) {
+    this.events[eventName] = [];
+  }
+
+  listenerCount(eventName) {
+    return this.events[eventName].length || 0;
+  }
+
+  // Additional methods:
+  eventNames() {
+    return Object.keys(this.events);
+  }
+
+  listeners(eventName) {
+    return this.events[eventName] || [];
+  }
+
+  addListener(eventName, callback) {
+    this.on(eventName, callback);
+  }
+
+  removeListener(eventName, callback) {
+    this.off(eventName, callback);
+  }
 }
+
+// Test it with with this one ;)
+const emitter = new MyEventEmitter();
+
+emitter.eventNames();
