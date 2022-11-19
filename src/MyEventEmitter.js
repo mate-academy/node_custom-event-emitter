a4d93c62687fedbddd62f7ade91ef718069f4702
Ihor Andrianov <andrianoia@gmail.com>
2022-11-19 13:58:46 +0200

    fix comments

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,80 +1,98 @@
-'use strict';
+"use strict";
 
-export class MyEventEmitter {
+class MyEventEmitter {
   constructor() {
-    this.events = new Map()
+    this.events = new Map();
   }
+
   on(event, callback) {
-    if (this.events.get(event)) {
-      const thisEvent = this.events.get(event)
-      this.events.delete(event)
-      this.events.set(event, [...thisEvent, callback])
-    } else {
-      this.events.set(event, [callback])
+    const thisEvent = this.events.get(event);
+    if (thisEvent) {
+      this.events.delete(event);
+      this.events.set(event, [...thisEvent, callback]);
+
+      return this;
     }
+
+    this.events.set(event, [callback]);
+
+    return this;
   }
+
   once(event, callback) {
     const callbackWrapper = () => {
-      callback()
-      this.off(event, callbackWrapper)
+      callback();
+      this.off(event, callbackWrapper);
+    };
+    const thisEvent = this.events.get(event);
+    if (thisEvent) {
+      this.events.delete(event);
+      this.events.set(event, [...thisEvent, callbackWrapper]);
+
+      return this;
     }
-    if (this.events.get(event)) {
-      const thisEvent = this.events.get(event)
-      this.events.delete(event)
-      this.events.set(event, [...thisEvent, callbackWrapper])
-    } else {
-      this.events.set(event, [callbackWrapper])
-    }
-    return this
+    this.events.set(event, [callbackWrapper]);
+
+    return this;
   }
+
   off(event, callback) {
-    const eventList = this.events
-      .get(event)
-      .filter((c) => c + "" !== callback + "")
-    this.events.delete(event)
-    this.events.set(event, eventList)
-    return this
+    const eventList = this.events.get(event).filter((c) => c !== callback);
+    this.events.delete(event);
+
+    this.events.set(event, eventList);
+
+    return this;
   }
+
   emit(event, ...args) {
     if (this.events.get(event)) {
-      this.events.get(event).forEach((c) => c(...args))
+      this.events.get(event).forEach((c) => c(...args));
       return true;
     }
     return false;
   }
+
   prependListener(event, callback) {
-    if (this.events.get(event)) {
-      const thisEvent = this.events.get(event)
-      this.events.delete(event)
-      this.events.set(event, [callback, ...thisEvent])
+    const thisEvent = this.events.get(event);
+
+    if (thisEvent) {
+      this.events.delete(event);
+      this.events.set(event, [callback, ...thisEvent]);
     } else {
-      this.events.set(event, [callback])
+      this.events.set(event, [callback]);
     }
-    return this
+    return this;
   }
+
   prependOnceListener(event, callback) {
     const callbackWrapper = () => {
-      callback()
-      this.off(event, callbackWrapper)
-    }
+      callback();
+      this.off(event, callbackWrapper);
+    };
 
     if (this.events.get(event)) {
-      const thisEvent = this.events.get(event)
-      this.events.delete(event)
-      this.events.set(event, [callbackWrapper, ...thisEvent])
+      const thisEvent = this.events.get(event);
+      this.events.delete(event);
+      this.events.set(event, [callbackWrapper, ...thisEvent]);
     } else {
-      this.events.set(event, [callbackWrapper])
+      this.events.set(event, [callbackWrapper]);
     }
-    return this
+    return this;
   }
+
   removeAllListeners() {
-    this.events.clear()
-    return this
+    this.events.clear();
+    return this;
   }
+
   listenerCount(event) {
-    if (this.events.get(event)) {
-      return this.events.get(event).length()
+    const thisEvent = this.events.get(event);
+    if (thisEvent) {
+      return thisEvent.length;
     }
     return 0;
   }
 }
+
+module.exports.MyEventEmitter = MyEventEmitter;
