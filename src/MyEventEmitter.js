58d004c87bf6fa2d4e74157341d57afa015f48f6
Pavlo Moroz <pmoroz2004@gmail.com>
2023-01-23 15:58:09 +0200

    my solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,138 @@
-'use strict';
-
 export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this._listeners = {};
+  }
+  _checkEventName(eventName) {
+    if (!eventName) {
+      throw new Error('Event name must be defined!');
+    }
+
+    if (typeof eventName !== 'string') {
+      throw new Error('Event name must be a string!');
+    }
+  }
+
+  _checkEventListener(listener) {
+    if (!listener) {
+      throw new Error('Event handler must be defined!');
+    }
+
+    if (typeof listener !== 'function') {
+      throw new Error('Event handler must be a function!');
+    }
+  }
+  on(name, func) {
+    this._checkEventName(name);
+    this._checkEventListener(func);
+
+    if (!this._listeners[name]) {
+      this._listeners[name] = [];
+    }
+
+    this._listeners[name].push(func);
+
+    return this;
+  }
+  once(name, func) {
+    this._checkEventName(name);
+    this._checkEventListener(func);
+
+    if (!this._listeners[name]) {
+      this._listeners[name] = [];
+    }
+
+    const onceWrapper = (...args) => {
+      func(...args);
+      this.off(name, onceWrapper);
+    };
+
+    this._listeners[name].push(onceWrapper);
+
+    return this;
+  }
+  off(name, func) {
+    this._checkEventName(name);
+    this._checkEventListener(func);
+
+    if (!this._listeners[name]) {
+      return this;
+    }
+
+    this._listeners[name] = this._listeners[name]
+      .filter(listener => listener !== func);
+
+    if (!this._listeners[name].length) {
+      delete this._listeners[name];
+    }
+
+    return this;
+  }
+  emit(name, ...args) {
+    this._checkEventName(name);
+
+    const listeners = this._listeners[name];
+
+    if (!listeners) {
+      return false;
+    }
+
+    for (const listener of listeners) {
+      listener(...args);
+    }
+
+    return true;
+  }
+  prependListener(name, func) {
+    this._checkEventName(name);
+    this._checkEventListener(func);
+
+    if (!this._listeners[name]) {
+      this._listeners[name] = [];
+    }
+
+    this._listeners[name].unshift(func);
+
+    return this;
+  }
+  prependOnceListener(name, func) {
+    this._checkEventName(name);
+    this._checkEventListener(func);
+
+    if (!this._listeners[name]) {
+      this._listeners[name] = [];
+    }
+
+    const onceWrapper = (...args) => {
+      func(...args);
+      this.off(name, onceWrapper);
+    };
+
+    this._listeners[name].unshift(onceWrapper);
+
+    return this;
+  }
+  removeAllListeners(name) {
+    if (!name) {
+      this._listeners = {};
+
+      return this;
+    }
+
+    this._checkEventName(name);
+
+    if (!this._listeners[name]) {
+      return this;
+    }
+
+    delete this._listeners[name];
+
+    return this;
+  }
+  listenerCount(name) {
+    this._checkEventName(name);
+
+    const listener = this._listeners[name] || [];
+
+    return listener.length;
+  }
 }
