2d6231c9bde2182e2f739c4d12186c9fc3bc1093
ysrckr <dyt.yasarcakir@gmail.com>
2023-02-15 13:27:43 +0300

    completed

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,64 @@
-'use strict';
-
 export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this._events = {};
+  }
+
+  on(name, listener) {
+    if (!this._events[name]) {
+      this._events[name] = [];
+    }
+
+    this._events[name].push(listener);
+  }
+
+  once(name, listener) {
+    const onceListener = (...args) => {
+      listener(...args);
+      this.off(name, onceListener);
+    };
+
+    this.on(name, onceListener);
+  }
+
+  off(name, listener) {
+    if (this._events[name]) {
+      this._events[name] = this._events[name].filter((fn) => fn !== listener);
+    }
+  }
+
+  emit(name, ...args) {
+    if (this._events[name]) {
+      this._events[name].forEach((fn) => fn(...args));
+    }
+  }
+
+  prependListener(name, listener) {
+    if (!this._events[name]) {
+      this._events[name] = [];
+    }
+
+    this._events[name].unshift(listener);
+  }
+
+  prependOnceListener(name, listener) {
+    const onceListener = (...args) => {
+      listener(...args);
+      this.off(name, onceListener);
+    };
+
+    this.prependListener(name, onceListener);
+  }
+
+  removeAllListeners([names]) {
+    if (names) {
+      names.forEach((name) => delete this._events[name]);
+    } else {
+      this._events = {};
+    }
+  }
+
+  listenerCount(name) {
+    return this._events[name] ? this._events[name].length : 0;
+  }
 }
+
