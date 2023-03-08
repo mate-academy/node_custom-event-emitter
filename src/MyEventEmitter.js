b5088492adcabc2d55ea6d3b9695f101ee9b5dcf
Anna Inozemets <inoznetta@gmail.com>
2023-03-08 19:39:53 +0200

    Custom implementation EventEmmiter methods was realised

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,113 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, listener) {
+    const { events } = this;
+
+    if (Object.prototype.hasOwnProperty.call(events, eventName)) {
+      events[eventName].push(listener);
+    } else {
+      events[eventName] = [];
+    }
+
+    return this;
+  }
+
+  once(eventName, listener) {
+    const newListener = (...args) => {
+      listener(...args);
+      this.removeListener(eventName, newListener);
+    };
+
+    this.on(eventName, newListener);
+
+    return this;
+  }
+
+  off(eventName, listener) {
+    const { events } = this;
+
+    events[eventName] = events[eventName]
+      .filter(currentListener => currentListener !== listener);
+
+    return this;
+  }
+
+  emit(eventName, ...args) {
+    const { events } = this;
+
+    const listeners = events[eventName];
+
+    if (!listeners) {
+      return false;
+    }
+
+    listeners.forEach(listener => listener(...args));
+
+    return true;
+  }
+
+  prependListener(eventName, listener) {
+    const { events } = this;
+
+    if (Object.prototype.hasOwnProperty.call(events, eventName)) {
+      events[eventName].unshift(listener);
+    } else {
+      events[eventName] = [];
+    }
+
+    return this;
+  }
+
+  prependOnceListener(eventName, listener) {
+    let hasAlredyCalled = false;
+
+    const newListener = (...args) => {
+      if (!hasAlredyCalled) {
+        hasAlredyCalled = true;
+        listener(...args);
+      } else {
+        this.removeListener(eventName, newListener);
+      }
+    };
+
+    return this.prependListener(eventName, newListener);
+  }
+
+  removeListener(eventName, listener) {
+    const { events } = this;
+
+    events[eventName] = events[eventName]
+      .filter(currentListener => currentListener !== listener);
+  }
+
+  removeAllListeners(eventName) {
+    let { events } = this;
+
+    if (eventName) {
+      events[eventName] = [];
+    } else {
+      events = {};
+    }
+  }
+
+  listenerCount(eventName) {
+    const { events } = this;
+
+    const listeners = events[eventName];
+
+    if (!listeners) {
+      return 0;
+    }
+
+    return listeners.length;
+  }
 }
+
+module.exports = {
+  MyEventEmitter,
+};
