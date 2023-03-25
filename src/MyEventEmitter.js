92688181dac92dd362e761821f9a35cf11d46eb7
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-03-25 19:28:52 +0200

    solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,121 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this._listeners = {};
+  }
+
+  _validateEventName(eventName) {
+    if (!eventName) {
+      throw new Error('The event name must be passed');
+    }
+
+    if (typeof eventName !== 'string' || typeof eventName !== 'symbol') {
+      throw new Error('Invalid event name type');
+    }
+  }
+
+  _validateListener(listener) {
+    if (!listener) {
+      throw new Error('The listener must be passed');
+    }
+
+    if (typeof listener !== 'function') {
+      throw new Error('Invalid listener type');
+    }
+  }
+
+  on(eventName, listener) {
+    this._validateEventName(eventName);
+    this._validateListener(listener);
+
+    if (!this._listeners.hasOwnProperty(eventName)) {
+      this._listeners[eventName] = [];
+    }
+
+    this._listeners[eventName].push(listener);
+
+    return this;
+  }
+
+  once(eventName, listener) {
+    this._validateEventName(eventName);
+    this._validateListener(listener);
+
+    const onceCallback = (...args) => {
+      listener(args);
+      this.off(eventName, onceCallback);
+    };
+
+    this.on(eventName, onceCallback);
+  }
+
+  off(eventName, listenerToRemove) {
+    this._validateEventName(eventName);
+    this._validateListener(listenerToRemove);
+
+    if (this._listeners.hasOwnProperty(eventName)) {
+      this._listeners[eventName] = this._listeners[eventName]
+        .filter(listener => listener !== listenerToRemove);
+    }
+
+    return this;
+  }
+
+  emit(eventName, ...args) {
+    this._validateEventName(eventName);
+
+    if (this._listeners.hasOwnProperty(eventName)) {
+      this._listeners[eventName].forEach(listener => listener(args));
+    }
+
+    return this;
+  }
+
+  prependListener(eventName, listener) {
+    this._validateEventName(eventName);
+    this._validateListener(listener);
+
+    if (!this._listeners.hasOwnProperty(eventName)) {
+      this._listeners[eventName] = [];
+    }
+
+    this._listeners[eventName].unshift(listener);
+
+    return this;
+  }
+
+  prependOnceListener(eventName, listener) {
+    this._validateEventName(eventName);
+    this._validateListener(listener);
+
+    const onceCallback = (...args) => {
+      listener(args);
+      this.off(eventName, onceCallback);
+    };
+
+    this.prependListener(eventName, onceCallback);
+  }
+
+  removeAllListeners(eventName) {
+    if (!eventName) {
+      this._listeners = {};
+
+      return this;
+    }
+
+    if (this._listeners.hasOwnProperty(eventName)) {
+      delete this._listeners[eventName];
+    }
+
+    return this;
+  }
+
+  listenerCount(eventName) {
+    this._validateEventName(eventName);
+
+    return this._listeners[eventName].length || 0;
+  }
 }
+
+module.exports = { MyEventEmitter };
