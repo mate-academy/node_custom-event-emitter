3cbde8dd06626035108391c8a2460bcb8e245e1a
OlhaArama <arama_o_f@ukr.net>
2023-04-03 18:08:00 +0300

    Solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,80 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  checkListenerType(listener) {
+    if (typeof listener !== 'function') {
+      throw new Error('Listener must be a function');
+    }
+  }
+
+  on(eventName, listener) {
+    this.checkListenerType(listener);
+
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].push(listener);
+  }
+
+  once(eventName, listener) {
+    this.checkListenerType(listener);
+
+    const onceWrapper = (...args) => {
+      listener(...args);
+
+      this.off(eventName, onceWrapper);
+    };
+
+    this.on(eventName, onceWrapper);
+  }
+
+  off(eventName, listenerForRemove) {
+    if (this.events[eventName]) {
+      this.events[eventName] = this.events[eventName]
+        .filter(listener => listener !== listenerForRemove);
+    }
+  }
+
+  emit(eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(listener => listener(data));
+    }
+  }
+
+  prependListener(eventName, listener) {
+    this.checkListenerType(listener);
+
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].unshift(listener);
+  }
+
+  prependOnceListener(eventName, listener) {
+    this.checkListenerType(listener);
+
+    const onceWrapper = (...args) => {
+      listener(...args);
+
+      this.off(eventName, onceWrapper);
+    };
+
+    this.prependListener(eventName, onceWrapper);
+  }
+
+  removeAllListeners(eventName) {
+    delete this.events[eventName];
+  }
+
+  listenerCount(eventName) {
+    return this.events[eventName].length || 0;
+  }
 }
+
+module.exports = { MyEventEmitter };
