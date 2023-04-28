03559b83a0234a825ea1aea42e397e497211abe1
timoxa2t <alex2t172@gmail.com>
2023-04-28 14:42:05 +0300

    Solution

---
+++ main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const MyEventEmitter = require('./src/MyEventEmitter');
+const myConsole = require('console');
+
+const myEmitter = new MyEventEmitter();
+
+myEmitter.on('todo', () => {
+  myConsole.log('todo event');
+});
+
+const callback = (...args) => {
+  myConsole.log('todo event', ...args);
+};
+
+myEmitter.on('not todo', callback);
+
+myEmitter.prependListener('todo', (...args) => {
+  myConsole.log('todo event prepend', ...args);
+});
+
+myEmitter.prependOnceListener('todo', (...args) => {
+  myConsole.log('todo event prepend once', ...args);
+});
+
+myEmitter.off('not todo', callback);
+
+myEmitter.emit('todo', 1);
+myEmitter.emit('not todo');
+myEmitter.emit('todo', 2);
+
+myConsole.log(myEmitter.listenerCount('todo'));
+myConsole.log(myEmitter.listenerCount('todo', callback));
+myConsole.log(myEmitter.listenerCount('not todo'));
+
+myEmitter.removeAllListeners();
+
+myConsole.log(myEmitter.listenerCount('todo'));
--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,94 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {
+
+    };
+  }
+
+  addEvent(name, callback, repeat, prepend) {
+    if ({}.hasOwnProperty.call(this.events, name)) {
+      if (prepend) {
+        this.events[name].unshift({
+          repeat,
+          callback,
+        });
+      } else {
+        this.events[name].push({
+          repeat,
+          callback,
+        });
+      }
+    } else {
+      this.events[name] = [
+        {
+          repeat,
+          callback,
+        },
+      ];
+    }
+  }
+
+  on(name, callback) {
+    this.addEvent(name, callback, true, false);
+  }
+
+  once(name, callback) {
+    this.addEvent(name, callback, false, false);
+  }
+
+  prependListener(name, callback) {
+    this.addEvent(name, callback, true, true);
+  }
+
+  prependOnceListener(name, callback) {
+    this.addEvent(name, callback, false, true);
+  }
+
+  off(name, listener) {
+    if ({}.hasOwnProperty.call(this.events, name)) {
+      const callbackIndex = this.events[name].findIndex(
+        ({ callback }) => callback === listener
+      );
+
+      if (callbackIndex !== -1) {
+        if (this.events[name].length === 1) {
+          delete this.events[name];
+        } else {
+          this.events[name].splice(callbackIndex, 1);
+        }
+      }
+    }
+  }
+
+  emit(name, ...args) {
+    if ({}.hasOwnProperty.call(this.events, name)) {
+      const callbacks = this.events[name];
+
+      callbacks.forEach(({ callback }) => {
+        callback(args);
+      });
+      this.events[name] = callbacks.filter(({ repeat }) => repeat);
+    }
+  }
+
+  removeAllListeners() {
+    this.events = [];
+  }
+  listenerCount(name, listener) {
+    if ({}.hasOwnProperty.call(this.events, name)) {
+      if (listener) {
+        return this.events[name].filter(
+          ({ callback }) => callback === listener
+        ).length;
+      } else {
+        return this.events[name].length;
+      }
+    }
+
+    return 0;
+  }
 }
+
+module.exports = MyEventEmitter;
