df24ba51e2f20189003abb8ff829753120f1429a
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-06-10 17:04:04 +0300

    Solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,88 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  };
+
+  on(eventName, listener) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].push(listener);
+  }
+
+  once(eventName, listener) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    const callback = (...args) => {
+      listener(...args);
+      this.off(eventName, callback);
+    };
+
+    this.events[eventName].push(callback);
+
+    return this;
+  }
+
+  off(eventName, listenerToRemove) {
+    if (!this.events[eventName]) {
+      throw new Error(`Event "${eventName}" doesn't exits.`);
+    }
+
+    this.events[eventName] = this.events[eventName]
+      .filter(listener => listener !== listenerToRemove);
+  }
+
+  emit(eventName, ...args) {
+    if (!this.events[eventName]) {
+      throw new Error(`Event "${eventName}" doesn't exits.`);
+    }
+
+    this.events[eventName].forEach((listener) => listener(args));
+  }
+
+  prependListener(eventName, listener) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].unshift(listener);
+  }
+
+  prependOnceListener(eventName, listener) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    const callback = (...args) => {
+      listener(...args);
+      this.off(eventName, callback);
+    };
+
+    this.prependListener(eventName, callback);
+  }
+
+  removeAllListeners(eventName) {
+    if (eventName) {
+      delete this.events[eventName];
+    } else {
+      this.events = {};
+    }
+  }
+
+  listenerCount(eventName) {
+    if (!this.events[eventName]) {
+      throw new Error(`Event "${eventName}" doesn't exits.`);
+    }
+
+    return this.events[eventName].length;
+  }
 }
+
+module.exports = {
+  MyEventEmitter,
+};
