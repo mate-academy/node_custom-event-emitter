576cfb130200ca56d9c4b71ab90709da3233d815
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-08-27 00:01:25 +0300

    All event emitter methods have been made

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,79 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, callBack) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].push(callBack);
+  }
+
+  once(eventName, callBack) {
+    const onceWrapper = (...args) => {
+      callBack.apply(this, args);
+
+      this.off(eventName, onceWrapper);
+    }
+
+    this.on(eventName, onceWrapper);
+  }
+
+  off(eventName, callBack) {
+    const eventCallBacks = this.events[eventName];
+
+    if (!eventCallBacks) return;
+
+    if (callBack) {
+      this.events[eventName] = eventCallBacks.filter(cb => cb !== callBack);
+    } else {
+      delete this.events[eventName];
+    }
+  }
+
+  emit(eventName, ...args) {
+    const eventCallBacks = this.events[eventName];
+
+    if (!eventCallBacks) return;
+
+    eventCallBacks.forEach(callBack => {
+      callBack.apply(this, args);
+    });
+  }
+
+  prependListener(eventName, callBack) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].unshift(callBack);
+  }
+
+  prependOnceListener(eventName, callBack) {
+    const prependOnceListenerWrapper = (...args) => {
+      callBack.apply(this, args);
+
+      this.off(eventName, prependOnceListenerWrapper);
+    }
+
+    this.prependListener(eventName, prependOnceListenerWrapper);
+  }
+
+  removeAllListeners(eventName = null) {
+    if (eventName !== null) {
+      delete this.events[eventName];
+    } else {
+      this.events = {};
+    }
+  }
+
+  listenerCount(eventName) {
+    const eventCallBacks = this.events[eventName];
+
+    return eventCallBacks ? eventCallBacks.length : 0;
+  }
 }
