ba1868f9e181a55c6c1f9411dfa3b03f79b6daa1
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-08-27 12:13:36 +0300

    Code has been fixed after review

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,16 +1,18 @@
 'use strict';
 
+/* eslint-disable no-console */
+
 class MyEventEmitter {
   constructor() {
     this.events = {};
   }
 
   on(eventName, callBack) {
-    if (!this.events[eventName]) {
-      this.events[eventName] = [];
-    }
+    this.events[eventName] = this.events[eventName] || [];
 
     this.events[eventName].push(callBack);
+
+    return this;
   }
 
   once(eventName, callBack) {
@@ -21,13 +23,15 @@ class MyEventEmitter {
     };
 
     this.on(eventName, onceWrapper);
+
+    return this;
   }
 
   off(eventName, callBack) {
     const eventCallBacks = this.events[eventName];
 
     if (!eventCallBacks) {
-      return;
+      throw new Error('Event you are trying to delete does not exist');
     }
 
     if (callBack) {
@@ -35,6 +39,8 @@ class MyEventEmitter {
     } else {
       delete this.events[eventName];
     }
+
+    return this;
   }
 
   emit(eventName, ...args) {
@@ -55,33 +61,51 @@ class MyEventEmitter {
     }
 
     this.events[eventName].unshift(callBack);
+
+    return this;
   }
 
   prependOnceListener(eventName, callBack) {
     const prependOnceListenerWrapper = (...args) => {
-      callBack.apply(this, args);
-
       this.off(eventName, prependOnceListenerWrapper);
+      callBack.apply(this, args);
     };
 
     this.prependListener(eventName, prependOnceListenerWrapper);
+
+    return this;
   }
 
   removeAllListeners(eventName = null) {
-    if (eventName !== null) {
+    if (eventName) {
       delete this.events[eventName];
     } else {
       this.events = {};
     }
+
+    return this;
   }
 
   listenerCount(eventName) {
-    const eventCallBacks = this.events[eventName];
-
-    return eventCallBacks ? eventCallBacks.length : 0;
+    return this.events[eventName] || 0;
   }
 }
 
 const myEmitter = new MyEventEmitter();
 
-myEmitter.listenerCount();
+myEmitter.once('test', () => console.log('This is first test'));
+myEmitter.once('test', () => console.log('This is second test'));
+myEmitter.once('test', () => console.log('This is third test'));
+
+myEmitter.on('test1', () => console.log('This is on event'))
+myEmitter.on('test1', () => console.log('This is second on event'))
+
+myEmitter.emit('test');
+myEmitter.emit('test');
+myEmitter.emit('test');
+
+myEmitter.emit('test1');
+myEmitter.emit('test1');
+myEmitter.emit('test1');
+
+myEmitter.listenerCount('greet');
