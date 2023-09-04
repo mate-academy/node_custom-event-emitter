bae1a28b76f77985f8a700f934af17459ed17d1e
John Rust <johnrust12@gmail.com>
2023-09-04 09:45:17 +0300

    Solution1

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,122 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  validateEventName(eventName) {
+    if (typeof eventName !== 'string') {
+      throw new Error('The event name supposed to be a string');
+    }
+  }
+
+  validateListener(listener) {
+    if (typeof listener !== 'function') {
+      throw new Error('The listener supposed to be a fanction');
+    }
+  }
+
+  on(eventName, listener) {
+    this.validateEventName(eventName);
+    this.validateListener(listener);
+
+    if (!(eventName in this.events)) {
+      this.events[eventName] = [listener];
+    } else {
+      this.events[eventName].push(listener);
+    }
+
+    return this;
+  }
+
+  once(eventName, listener) {
+    this.validateEventName(eventName);
+    this.validateListener(listener);
+
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    const onceFn = (...args) => {
+      listener(args);
+      this.removeListener(eventName, onceFn);
+    };
+
+    this.events[eventName].push(onceFn);
+
+    return this;
+  }
+
+  off(eventName, listener) {
+    this.validateEventName(eventName);
+    this.validateListener(listener);
+
+    this.events[eventName] = this.events[eventName].filter(eventListener =>
+      eventListener !== listener
+    );
+
+    return this;
+  }
+
+  emit(eventName, ...args) {
+    this.validateEventName(eventName);
+
+    if (!this.events[eventName]) {
+      return false;
+    }
+
+    this.events[eventName].forEach(listener => {
+      listener(...args);
+    });
+
+    return true;
+  };
+
+  prependListener(eventName, listener) {
+    this.validateEventName(eventName);
+    this.validateListener(listener);
+
+    if (!(eventName in this.events)) {
+      this.events[eventName] = [listener];
+    } else {
+      this.events[eventName].unshift(listener);
+    }
+
+    return this;
+  }
+
+  prependOnceListener(eventName, listener) {
+    this.validateEventName(eventName);
+    this.validateListener(listener);
+
+    const onceList = (...args) => {
+      listener(...args);
+      this.removeListener(eventName, onceList);
+    };
+
+    if (!(eventName in this.events)) {
+      this.events[eventName] = [onceList];
+    } else {
+      this.events[eventName].unshift(onceList);
+    }
+
+    return this;
+  }
+
+  removeAllListeners(eventName) {
+    this.validateEventName(eventName);
+
+    delete this.events[eventName];
+
+    return this;
+  }
+
+  listenerCount(eventName) {
+    this.validateEventName(eventName);
+
+    return this.events[eventName].length || 0;
+  }
 }
+
+module.exports = { MyEventEmitter };
