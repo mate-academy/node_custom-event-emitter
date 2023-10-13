afa21f0924ef4356ae8658267b22d537c96500d3
KatOlista^^! <olya.katyushina@gmail.com>
2023-10-13 18:43:08 +0200

    implement emitter

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,83 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+
+    this.events[event].push(callback);
+
+    return this;
+  }
+
+  once(event, callback) {
+    const newCallback = (args) => {
+      callback(args);
+      this.off(event, newCallback);
+    };
+
+    this.on(event, newCallback);
+
+    return this;
+  }
+
+  off(event, callback) {
+    if (!this.events[event]) {
+      return this;
+    }
+
+    this.events[event].filter(func => func !== callback);
+
+    return this;
+  }
+
+  emit(event, ...args) {
+    this.events[event].forEach(callback => callback(args));
+
+    return this;
+  }
+
+  prependListener(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+
+    this.events[event].unshift(callback);
+
+    return this;
+  }
+
+  prependOnceListener(event, callback) {
+    const newCallback = (args) => {
+      callback(args);
+      this.off(event, newCallback);
+    };
+
+    this.prependListener(event, newCallback);
+
+    return this;
+  }
+
+  removeAllListeners(event) {
+    if (this.events[event]) {
+      this.events[event] = null;
+    }
+  }
+
+  listenerCount(event) {
+    if (this.events[event]) {
+      return this.events[event].length;
+    }
+
+    return 0;
+  }
 }
+
+module.exports = {
+  MyEventEmitter,
+};
