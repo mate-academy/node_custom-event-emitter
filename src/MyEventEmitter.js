2daa7c7fe61adb19763d164fff39c904f377c252
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-10-14 12:24:58 +0300

    add task solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,105 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, listener) {
+    if (this.events[eventName]) {
+      this.events[eventName].listeners.push({ listener });
+    } else {
+      this.events[eventName] = {
+        listeners: [{ listener }],
+      };
+    }
+
+    return this;
+  }
+  once(eventName, listener) {
+    if (this.events[eventName]) {
+      this.events[eventName].listeners.push({
+        listener, triggerOnce: true,
+      });
+    } else {
+      this.events[eventName] = {
+        listeners: [{
+          listener, triggerOnce: true,
+        }],
+      };
+    }
+
+    return this;
+  }
+  off(eventName, listener) {
+    if (this.events[eventName]) {
+      const listenersArr = this.events[eventName].listeners;
+
+      this.events[eventName].listeners = listenersArr
+        .filter((item) => item !== listener);
+    }
+  }
+  emit(eventName, ...args) {
+    const event = this.events[eventName];
+
+    if (event) {
+      event.listeners.forEach((item) => {
+        item.listener(...args);
+
+        if (item.triggerOnce) {
+          this.off(eventName, item);
+        }
+      });
+    }
+
+    return this;
+  }
+  prependListener(eventName, listener) {
+    if (this.events[eventName]) {
+      this.events[eventName].listeners.unshift({ listener });
+    } else {
+      this.events[eventName] = {
+        listeners: [{ listener }],
+      };
+    }
+
+    return this;
+  }
+  prependOnceListener(eventName, listener) {
+    if (this.events[eventName]) {
+      this.events[eventName].listeners.unshift({
+        listener, triggerOnce: true,
+      });
+    } else {
+      this.events[eventName] = {
+        listeners: [{
+          listener, triggerOnce: true,
+        }],
+      };
+    }
+
+    return this;
+  }
+  removeAllListeners(eventName) {
+    if (eventName) {
+      delete this.events[eventName];
+    } else {
+      this.events = {};
+    }
+
+    return this;
+  }
+  listenerCount(eventName) {
+    const event = this.events[eventName];
+
+    if (event) {
+      return event.listeners.length;
+    }
+
+    return 0;
+  }
 }
+
+module.exports = {
+  MyEventEmitter,
+};
