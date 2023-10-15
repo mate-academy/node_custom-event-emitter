8d07feb957b1f00530997770c18bb3afc49ad974
Kate <ekaterina2002aleks@gmail.com>
2023-10-15 22:56:01 +0300

    Solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -4,16 +4,67 @@ const http = require('http');
 const server = new http.Server();
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
+  constructor() {
+    this.events = {};
+  }
+  on(eventName, listener) {
+    if (eventName in this.events) {
+      this.events[eventName].push(listener);
+    } else {
+      this.events[eventName] = [listener];
+    }
+
+    return this;
+  }
+  once(eventName, listener) {
+    if (eventName in this.events) {
+      this.events[eventName].push(listener);
+    } else {
+      this.events[eventName] = [listener];
+    }
+
+    return this;
+  }
+  off(eventName, listener) {
+    this.events[eventName] = this.events[eventName].filter(el => (
+      el !== listener
+    ));
+
+    return this;
+  }
   emit(eventName, ...args) {
     http.Server.prototype.emit.call(server, eventName, ...args);
+
+    return this;
   }
-  prependListener() {}
+  prependListener(eventName, listener) {
+    if (eventName in this.events) {
+      this.events[eventName].unshift(listener);
+    } else {
+      this.events[eventName] = [listener];
+    }
+
+    return this;
+  }
   prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  removeAllListeners(eventName) {
+    if (eventName) {
+      this.events[eventName].length = 0;
+    } else {
+      for (const key in this.events) {
+        this.events[key].length = 0;
+      }
+    }
+
+    return this;
+  }
+  listenerCount(eventName, listener) {
+    if (listener) {
+      return this.events[eventName].filter(el => el === listener).length;
+    } else {
+      return this.events[eventName].length;
+    }
+  }
 }
 
 module.exports = {
