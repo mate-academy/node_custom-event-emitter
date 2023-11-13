4580a0d812fcdbb3d04c0b843f5cbfb33f1c8518
Zibi95 <sylwester.sob95@gmail.com>
2023-11-13 14:20:55 +0100

    solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,90 @@
-'use strict';
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  on(eventName, callback) {
+    const listeners = this.events[eventName];
+
+    if (listeners) {
+      listeners.push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+
+    return this;
+  }
+  once(eventName, callback) {
+    const listeners = this.events[eventName];
+
+    const onceCallback = () => {
+      callback();
+      this.off(eventName, onceCallback);
+    };
+
+    if (listeners) {
+      listeners.push(onceCallback);
+    } else {
+      this.events[eventName] = [onceCallback];
+    }
+
+    return this;
+  }
+  off(eventName, callback) {
+    const listenerIndex = this.events[eventName].indexOf(callback);
+
+    if (listenerIndex >= 0) {
+      this.events[eventName].splice(listenerIndex, 1);
+    }
+
+    return this;
+  }
+  emit(eventName, ...args) {
+    const listeners = this.events[eventName];
+
+    if (listeners) {
+      listeners.forEach((listener) => listener(...args));
+    }
+
+    return this;
+  }
+  prependListener(eventName, callback) {
+    const listeners = this.events[eventName];
+
+    if (listeners) {
+      listeners.unshift(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+
+    return this;
+  }
+  prependOnceListener(eventName, callback) {
+    const listeners = this.events[eventName];
+
+    const onceCallback = () => {
+      callback();
+      this.off(eventName, onceCallback);
+    };
+
+    if (listeners) {
+      listeners.unshift(onceCallback);
+    } else {
+      this.events[eventName] = [onceCallback];
+    }
+
+    return this;
+  }
+  removeAllListeners(eventName) {
+    delete this.events[eventName];
+
+    return this;
+  }
+  listenerCount(eventName) {
+    return this.events[eventName].length;
+  }
 }
+
+module.exports = {
+  MyEventEmitter,
+};
