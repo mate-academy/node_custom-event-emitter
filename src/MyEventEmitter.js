0a2d91fa47861522ea34be4948cc6d1831fc7d27
Dmytro <dmytro2105@gmail.com>
2023-12-11 00:25:54 +0200

    MYEventEmitter

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -3,65 +3,86 @@
 class MyEventEmitter {
   constructor() {
     this.listeners = new Map();
+  }
 
-    const onceWrapper = (eventName, listener) => {
-      listener();
-      this.off(eventName, onceWrapper);
+  static onceWrapper(eventName, listener, offMethod) {
+    const wrappedListener = (...args) => {
+      listener(...args);
+      offMethod(eventName, wrappedListener);
     };
 
-    this.on = (eventName, listener) => {
-      const existingListeners = this.listeners.get(eventName) || [];
+    return wrappedListener;
+  }
 
-      existingListeners.push(listener);
-      this.listeners.set(eventName, existingListeners);
-    };
+  on(eventName, listener) {
+    const existingListeners = this.listeners.get(eventName) || [];
 
-    this.once = (eventName, listener) => {
-      this.on(eventName, onceWrapper(eventName, listener));
-    };
+    existingListeners.push(listener);
+    this.listeners.set(eventName, existingListeners);
+  }
 
-    this.off = (eventName, listener) => {
-      const existingListeners = this.listeners.get(eventName);
+  once(eventName, listener) {
+    const wrapper = MyEventEmitter.onceWrapper(
+      eventName,
+      listener,
+      this.off.bind(this) // Передаємо метод off як параметр
+    );
 
-      if (existingListeners) {
-        const index = existingListeners.indexOf(listener);
+    this.on(eventName, wrapper);
+  }
 
-        if (index !== -1) {
-          existingListeners.splice(index, 1);
-        }
+  off(eventName, listener) {
+    const existingListeners = this.listeners.get(eventName);
+
+    if (existingListeners) {
+      const index = existingListeners.indexOf(listener);
+
+      if (index !== -1) {
+        existingListeners.splice(index, 1);
       }
-    };
+    }
+  }
 
-    this.emit = (eventName, ...args) => {
-      const existingListeners = this.listeners.get(eventName);
+  emit(eventName, ...args) {
+    const existingListeners = this.listeners.get(eventName);
 
-      if (existingListeners) {
-        existingListeners.forEach(listener => {
-          listener(...args);
-        });
+    if (existingListeners) {
+      existingListeners.forEach((listener) => {
+        listener(...args);
+      });
+    }
+  }
+
+  prependListener(eventName, listener) {
+    this.listeners.set(eventName, [
+      listener,
+      ...(this.listeners.get(eventName) || []),
+    ]);
+  }
+
+  prependOnceListener(eventName, listener) {
+    this.prependListener(
+      eventName,
+      MyEventEmitter.onceWrapper(
+        eventName,
+        listener,
+        this.off.bind(this)
+      )
+    );
+  }
+
+  removeAllListeners(...eventName) {
+    if (!eventName.length) {
+      this.listeners.clear();
+    } else {
+      for (const name of eventName) {
+        this.listeners.delete(name);
       }
-    };
+    }
+  }
 
-    this.prependListener = (eventName, listener) => {
-      this.listeners.set(eventName,
-        [listener, ...this.listeners.get(eventName) || []]);
-    };
-
-    this.prependOnceListener = (eventName, listener) => {
-      this.prependListener(eventName, onceWrapper(eventName, listener));
-    };
-
-    this.removeAllListeners = (...eventName) => {
-      if (!eventName) {
-        this.listeners.clear();
-      } else {
-        delete this.listeners.get(eventName);
-      }
-    };
-
-    this.listenerCount = (eventName) => {
-      return this.listeners.get(eventName).length;
-    };
+  listenerCount(eventName) {
+    return this.listeners.get(eventName).length || 0;
   }
 }
 
