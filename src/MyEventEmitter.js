104beb58dc03c05f0ed45ee87bbf9c60d8f318c8
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-05-15 14:53:48 +0300

    solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,67 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this.events = [];
+  }
+
+  on(eventName, listener) {
+    this.events.push({
+      eventName,
+      listener,
+    });
+  }
+
+  once(eventName, listener) {
+    this.events.push({
+      eventName,
+      listener,
+      once: true,
+    });
+  }
+
+  off(eventName, listener) {
+    this.events = this.events.filter(
+      (events) =>
+        !(eventName === events.eventName && listener === events.listener),
+    );
+  }
+
+  emit(event, ...args) {
+    this.events = this.events.filter(({ eventName, listener, once }) => {
+      if (event === eventName) {
+        listener(...args);
+
+        return !once;
+      }
+
+      return true;
+    });
+  }
+
+  prependListener(eventName, listener) {
+    this.events.unshift({
+      eventName,
+      listener,
+    });
+  }
+
+  prependOnceListener(eventName, listener) {
+    this.events.unshift({
+      eventName,
+      listener,
+      once: true,
+    });
+  }
+
+  removeAllListeners(event) {
+    this.events = this.events.filter(({ eventName }) => event !== eventName);
+  }
+
+  listenerCount(eventName) {
+    return this.events.filter((listener) => listener.eventName === eventName)
+      .length;
+  }
 }
 
 module.exports = MyEventEmitter;
