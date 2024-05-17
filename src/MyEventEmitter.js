9eeed64803d395cb2c029365c6be651a3d8e9c34
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-05-17 13:03:15 +0300

    solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,78 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  events = {};
+
+  on(type, callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('Listener must be a function!');
+    }
+
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(callback);
+  }
+
+  once(type, callback) {
+    const tempCallback = (...args) => {
+      callback(...args);
+
+      this.off(type, tempCallback);
+    };
+
+    this.on(type, tempCallback);
+  }
+
+  off(type, callback) {
+    if (this.events[type]) {
+      this.events[type].forEach((listener, index) => {
+        if (listener === callback) {
+          this.events[type] = this.events[type].filter((_, i) => i !== index);
+        }
+      });
+    }
+  }
+
+  emit(type, ...args) {
+    if (this.events[type]) {
+      this.events[type].forEach((callback) => {
+        callback(...args);
+      });
+    }
+  }
+
+  prependListener(type, callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('Listener must be a function!');
+    }
+
+    this.events[type] = this.events[type] || [];
+    this.events[type].unshift(callback);
+  }
+
+  prependOnceListener(type, callback) {
+    const tempCallback = (...args) => {
+      callback(...args);
+
+      this.off(type, tempCallback);
+    };
+
+    this.prependListener(type, tempCallback);
+  }
+
+  removeAllListeners(type) {
+    if (this.events[type]) {
+      delete this.events[type];
+    }
+  }
+
+  listenerCount(type) {
+    let callbackCont = 0;
+    const callbacks = this.events[type] || [];
+
+    callbackCont = callbacks.length;
+
+    return callbackCont;
+  }
 }
 
 module.exports = MyEventEmitter;
