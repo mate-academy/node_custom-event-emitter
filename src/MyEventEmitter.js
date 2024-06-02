bbe1b04cebc81b7fda73b1e383e7d20e01133008
Andrii Khomiak <an.khomiak@gmail.com>
2024-06-02 10:30:02 +0300

    task done

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,50 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  listeners = {};
+
+  on(event, callback) {
+    this.listeners[event] = this.listeners[event]
+      ? [...this.listeners[event], { callback, isOnce: false }]
+      : [{ callback, isOnce: false }];
+  }
+  once(event, callback) {
+    this.listeners[event] = this.listeners[event]
+      ? [...this.listeners[event], { callback, isOnce: true }]
+      : [{ callback, isOnce: true }];
+  }
+  off(event, callback) {
+    this.listeners[event] = this.listeners[event].filter(
+      (listener) => listener.callback !== callback,
+    );
+  }
+  emit(event, ...args) {
+    this.listeners[event].forEach((item) => item.callback(...args));
+
+    this.listeners[event] = this.listeners[event].filter(
+      (item) => item.isOnce === false,
+    );
+  }
+  prependListener(event, callback) {
+    this.listeners[event] = this.listeners[event]
+      ? [{ callback, isOnce: false }, ...this.listeners[event]]
+      : [{ callback, isOnce: false }];
+  }
+  prependOnceListener(event, callback) {
+    this.listeners[event] = this.listeners[event]
+      ? [{ callback, isOnce: true }, ...this.listeners[event]]
+      : [{ callback, isOnce: true }];
+  }
+  removeAllListeners(event = null) {
+    if (!event) {
+      this.listeners = {};
+    } else {
+      this.listeners[event] = [];
+    }
+  }
+  listenerCount(event) {
+    return this.listeners[event] ? this.listeners[event].length : 0;
+  }
 }
 
 module.exports = MyEventEmitter;
