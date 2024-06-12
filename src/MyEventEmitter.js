f83c683166a764c0743fe691f0970e7c925cc1f3
Ярина Пугач <puhachyaryna@gmail.com>
2024-06-12 13:04:23 +0200

    solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,69 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push({ callback, isOnce: false });
+  }
+
+  once(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push({ callback, isOnce: true });
+  }
+
+  off(event, callback) {
+    if (this.listeners[event]) {
+      this.listeners[event] = this.listeners[event].filter(
+        (listener) => listener.callback !== callback,
+      );
+    }
+  }
+
+  emit(event, ...args) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach((listener) => {
+        listener.callback(...args);
+      });
+
+      this.listeners[event] = this.listeners[event].filter(
+        (listener) => !listener.isOnce,
+      );
+    }
+  }
+
+  prependListener(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].unshift({ callback, isOnce: false });
+  }
+
+  prependOnceListener(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].unshift({ callback, isOnce: true });
+  }
+
+  removeAllListeners(event = null) {
+    if (event === null) {
+      this.listeners = {};
+    } else if (this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+  }
+
+  listenerCount(event) {
+    return this.listeners[event] ? this.listeners[event].length : 0;
+  }
 }
 
 module.exports = MyEventEmitter;
