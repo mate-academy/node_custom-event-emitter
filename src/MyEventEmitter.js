3f8c615657c1d18b642228c1ef72378b2e3e0da0
Andrii Rykal <andreuka79@gmail.com>
2025-04-27 15:16:42 +0300

    Solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,92 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(eventName, listener) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+
+    this.listeners[eventName].push(listener);
+
+    return this;
+  }
+
+  once(eventName, listener) {
+    const wrapper = (...args) => {
+      this.off(eventName, wrapper);
+      listener(...args);
+    };
+
+    wrapper.listener = listener;
+    this.on(eventName, wrapper);
+
+    return this;
+  }
+
+  off(eventName, listener) {
+    if (!this.listeners[eventName]) {
+      return this;
+    }
+
+    this.listeners[eventName] = this.listeners[eventName].filter((l) => {
+      return l !== listener && l.listener !== listener;
+    });
+
+    return this;
+  }
+
+  emit(eventName, ...args) {
+    if (!this.listeners[eventName]) {
+      return this;
+    }
+
+    this.listeners[eventName].forEach((listener) => {
+      listener(...args);
+    });
+
+    return this;
+  }
+
+  prependListener(eventName, listener) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+
+    this.listeners[eventName].unshift(listener);
+
+    return this;
+  }
+
+  prependOnceListener(eventName, listener) {
+    const wrapper = (...args) => {
+      this.off(eventName, wrapper);
+      listener(...args);
+    };
+
+    wrapper.listener = listener;
+
+    this.prependListener(eventName, wrapper);
+
+    return this;
+  }
+
+  removeAllListeners(eventName) {
+    if (eventName === undefined) {
+      this.listeners = {};
+    } else {
+      delete this.listeners[eventName];
+    }
+
+    return this;
+  }
+
+  listenerCount(eventName) {
+    return this.listeners[eventName]?.length || 0;
+  }
 }
 
 module.exports = MyEventEmitter;
