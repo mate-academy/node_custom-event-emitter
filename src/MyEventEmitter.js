100ac23d772a99047d04fa873b4406213701b77e
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2025-05-26 18:18:49 +0300

    solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,87 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  listeners = {};
+
+  on(eventName, func) {
+    if (!(eventName in this.listeners)) {
+      this.listeners[eventName] = [func];
+    } else {
+      this.listeners[eventName].push(func);
+    }
+  }
+
+  once(eventName, func) {
+    const onceFunc = (...args) => {
+      func(...args);
+      this.off(eventName, onceFunc);
+    };
+
+    this.on(eventName, onceFunc);
+
+    return this;
+  }
+
+  off(eventName, func) {
+    if (this.listeners[eventName]) {
+      const index = this.listeners[eventName].indexOf(func);
+
+      if (index !== -1) {
+        this.listeners[eventName].splice(index, 1);
+      }
+    }
+
+    return this;
+  }
+
+  emit(eventName, ...args) {
+    if (!this.listeners[eventName]) {
+      return;
+    }
+
+    const listenersToCall = [...this.listeners[eventName]];
+
+    listenersToCall.forEach((func) => {
+      func(...args);
+    });
+  }
+
+  prependListener(eventName, func) {
+    if (!(eventName in this.listeners)) {
+      this.listeners[eventName] = [func];
+    } else {
+      this.listeners[eventName].unshift(func);
+    }
+  }
+
+  prependOnceListener(eventName, func) {
+    const onceFunc = (...args) => {
+      func(...args);
+      this.off(eventName, onceFunc);
+    };
+
+    this.prependListener(eventName, onceFunc);
+
+    return this;
+  }
+
+  removeAllListeners(eventName) {
+    if (!eventName) {
+      this.listeners = {};
+    } else {
+      delete this.listeners[eventName];
+    }
+
+    return this;
+  }
+
+  listenerCount(eventName) {
+    if (this.listeners[eventName]) {
+      return this.listeners[eventName].length;
+    } else {
+      return 0;
+    }
+  }
 }
 
 module.exports = MyEventEmitter;
