4bf1c09a190de317feeb3ef2e6b3a5a0a81ffc18
Nadiia Bulmak <nadgaval@gmail.com>
2025-09-02 17:43:33 +0300

    custom event-emitter

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,65 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, listener) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(listener);
+  }
+  once(eventName, listener) {
+    const wrapper = (...args) => {
+      listener(...args);
+      this.off(eventName, wrapper);
+    };
+
+    this.on(eventName, wrapper);
+  }
+
+  off(eventName, listener) {
+    if (this.events[eventName]) {
+      this.events[eventName] = this.events[eventName].filter(
+        (l) => l !== listener,
+      );
+    }
+  }
+  emit(eventName, ...args) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach((listener) => listener(...args));
+    }
+  }
+  prependListener(eventName, listener) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].unshift(listener);
+  }
+  prependOnceListener(eventName, listener) {
+    const wrapper = (...args) => {
+      listener(...args);
+      this.off(eventName, wrapper);
+    };
+
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].unshift(wrapper);
+  }
+  removeAllListeners(eventName) {
+    if (eventName) {
+      delete this.events[eventName];
+    } else {
+      Object.keys(this.events).forEach((key) => delete this.events[key]);
+    }
+  }
+  listenerCount(eventName) {
+    return this.events[eventName] ? this.events[eventName].length : 0;
+  }
 }
 
 module.exports = MyEventEmitter;
