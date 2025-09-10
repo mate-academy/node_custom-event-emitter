9b53991b75f456e9ff15288052046bbc14924608
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-09-10 10:23:38 +0200

    add required changes

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -3,8 +3,13 @@
 class MyEventEmitter {
   constructor() {
     this.events = {};
+    this.onceMap = new WeakMap();
   }
   on(event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
+    }
+
     if (!this.events[event]) {
       this.events[event] = [];
     }
@@ -13,37 +18,57 @@ class MyEventEmitter {
     return this;
   }
   once(event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
+    }
+
     if (!this.events[event]) {
       this.events[event] = [];
     }
 
     const wrapper = (...args) => {
-      listener(...args);
+      listener.apply(this, args);
 
-      const filteredEvents = this.events[event].filter((ev) => ev !== wrapper);
-
-      this.events[event] = filteredEvents;
+      this.events[event] = this.events[event].filter(
+        (ev) => ev !== listener && ev !== wrapper,
+      );
+      this.onceMap.delete(listener);
     };
 
+    this.onceMap.set(listener, wrapper);
     this.events[event].push(wrapper);
+
+    return this;
   }
   off(event, listener) {
     if (!this.events[event]) {
       return;
     }
 
-    this.events[event] = this.events[event].filter((ev) => ev !== listener);
+    const wrapper = this.onceMap.get(listener);
+
+    this.events[event] = this.events[event].filter(
+      (ev) => ev !== listener && ev !== wrapper,
+    );
   }
   emit(event, ...args) {
     if (!this.events[event]) {
       return false;
     }
 
-    this.events[event].forEach((ev) => ev(...args));
+    const listeners = this.events[event].slice();
+
+    for (const listener of listeners) {
+      listener(...args);
+    }
 
     return true;
   }
   prependListener(event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
+    }
+
     if (!this.events[event]) {
       this.events[event] = [];
     }
@@ -52,12 +77,22 @@ class MyEventEmitter {
     return this;
   }
   prependOnceListener(event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
+    }
+
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+
     const wrapper = (...args) => {
-      listener(...args);
+      listener.apply(this, args);
 
       const filteredEvents = this.events[event].filter((ev) => ev !== wrapper);
 
       this.events[event] = filteredEvents;
+
+      return this;
     };
 
     this.events[event].unshift(wrapper);
@@ -65,7 +100,7 @@ class MyEventEmitter {
     return this;
   }
   removeAllListeners(event) {
-    if (!event) {
+    if (event === undefined) {
       this.events = {};
 
       return this;
