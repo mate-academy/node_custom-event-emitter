50172a083b67bd35658b7d1d6daf9bb476303dfb
Maria Hula <maria.gula0307@gmail.com>
2025-09-11 18:47:47 +0300

    Solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,70 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  listeners = [];
+
+  on(eventName, listener) {
+    this.listeners.push([eventName, listener, false, this.listeners.length]);
+
+    return this;
+  }
+  once(eventName, listener) {
+    this.listeners.push([eventName, listener, true, this.listeners.length]);
+
+    return this;
+  }
+  off(eventName, listener) {
+    this.listeners.splice(
+      this.listeners.findIndex(
+        ([name, curListener]) =>
+          name === eventName && curListener.toString() === listener.toString(),
+      ),
+      1,
+    );
+
+    return this;
+  }
+  emit(eventName, ...args) {
+    const onceListeners = [];
+    let foundListener = false;
+
+    for (let i = 0; i < this.listeners.length; i++) {
+      const [name, listener, isOnce, id] = this.listeners[i];
+
+      if (name === eventName) {
+        listener(...args);
+        foundListener = true;
+
+        if (isOnce) {
+          onceListeners.push(id);
+        }
+      }
+    }
+
+    this.listeners = this.listeners.filter(
+      ([, , , id]) => !onceListeners.includes(id),
+    );
+
+    return foundListener;
+  }
+  prependListener(eventName, listener) {
+    this.listeners.unshift([eventName, listener, false, this.listeners.length]);
+
+    return this;
+  }
+  prependOnceListener(eventName, listener) {
+    this.listeners.unshift([eventName, listener, true, this.listeners.length]);
+
+    return this;
+  }
+  removeAllListeners(eventName) {
+    this.listeners = this.listeners.filter(([name]) => name !== eventName);
+
+    return this;
+  }
+  listenerCount(eventName) {
+    return this.listeners.filter(([name]) => name === eventName).length;
+  }
 }
 
 module.exports = MyEventEmitter;
