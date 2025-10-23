35ff2ddeac423fe64c1f68a316920495de2be4be
Anton Kuchmasov <antek.ukraine@gmail.com>
2025-10-23 22:39:21 +0300

    finished MyEventEmitter implementation

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,109 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  listenersMap = new Map();
+  onceListenersMap = new Map();
+
+  on(event, callback) {
+    let updatedCallbacksArray = this.listenersMap.get(event);
+
+    if (!updatedCallbacksArray) {
+      updatedCallbacksArray = [];
+    }
+
+    updatedCallbacksArray.push(callback);
+
+    this.listenersMap.set(event, updatedCallbacksArray);
+  }
+
+  once(event, callback) {
+    let updatedOnceCallbacksArray = this.onceListenersMap.get(event);
+
+    if (!updatedOnceCallbacksArray) {
+      updatedOnceCallbacksArray = [];
+    }
+
+    this.on(event, callback);
+
+    updatedOnceCallbacksArray.push(callback);
+    this.onceListenersMap.set(event, updatedOnceCallbacksArray);
+  }
+
+  off(event, callback) {
+    let updatedCallbacksArray = this.listenersMap.get(event);
+
+    if (!updatedCallbacksArray || !updatedCallbacksArray.includes(callback)) {
+      return;
+    }
+
+    updatedCallbacksArray = updatedCallbacksArray.filter(
+      (cal) => cal !== callback,
+    );
+
+    this.listenersMap.set(event, updatedCallbacksArray);
+  }
+
+  emit(event, ...args) {
+    const callbacksToCall = this.listenersMap.get(event);
+    const onceCallbacksForEvent = this.onceListenersMap.get(event) || [];
+
+    if (!callbacksToCall || !callbacksToCall.length) {
+      return;
+    }
+
+    for (const cal of callbacksToCall) {
+      cal(...args);
+    }
+
+    for (const cal of callbacksToCall) {
+      if (onceCallbacksForEvent.includes(cal)) {
+        this.off(event, cal);
+      }
+    }
+
+    this.onceListenersMap.set(event, []);
+  }
+
+  prependListener(event, callback) {
+    let updatedCallbacksArray = this.listenersMap.get(event);
+
+    if (!updatedCallbacksArray) {
+      updatedCallbacksArray = [];
+    }
+
+    updatedCallbacksArray.unshift(callback);
+  }
+
+  prependOnceListener(event, callback) {
+    let updatedOnceCallbacksArray = this.onceListenersMap.get(event);
+
+    if (!updatedOnceCallbacksArray) {
+      updatedOnceCallbacksArray = [];
+    }
+
+    this.prependListener(event, callback);
+
+    updatedOnceCallbacksArray.unshift(callback);
+    this.onceListenersMap.set(event, updatedOnceCallbacksArray);
+  }
+
+  removeAllListeners(event) {
+    if (!event) {
+      this.listenersMap.clear();
+
+      return;
+    }
+
+    this.listenersMap.delete(event);
+  }
+
+  listenerCount(event) {
+    if (!this.listenersMap.get(event)) {
+      return 0;
+    }
+
+    return this.listenersMap.get(event).length;
+  }
 }
 
 module.exports = MyEventEmitter;
