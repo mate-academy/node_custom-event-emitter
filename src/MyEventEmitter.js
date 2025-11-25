ada4989540a6d3378649bff30134dab4b4e45384
Nikita Doroshenko <nikitokd05@gmail.com>
2025-11-25 19:52:01 +0200

    task solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,100 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  events = {};
+
+  on(eventName, listener) {
+    if (typeof listener !== 'function') {
+    } else {
+      if (!this.events[eventName]) {
+        this.events[eventName] = [];
+      }
+
+      this.events[eventName].push({ fn: listener, once: false });
+    }
+  }
+  once(eventName, listener) {
+    if (typeof listener !== 'function') {
+    } else {
+      if (!this.events[eventName]) {
+        this.events[eventName] = [];
+      }
+
+      this.events[eventName].push({ fn: listener, once: true });
+    }
+  }
+  off(eventName, listener) {
+    if (!this.events[eventName]) {
+    } else {
+      if (listener === undefined) {
+        this.events[eventName] = [];
+
+        return;
+      }
+
+      this.events[eventName] = this.events[eventName].filter(
+        (object) => object.fn !== listener,
+      );
+
+      if (this.events[eventName].length === 0) {
+        delete this.events[eventName];
+      }
+    }
+  }
+  emit(eventName, ...args) {
+    if (!this.events[eventName]) {
+    } else {
+      const listEvent = (this.events[eventName] || []).concat();
+
+      for (const object of listEvent) {
+        if (object && typeof object.fn === 'function') {
+          object.fn(...args);
+        }
+
+        if (object.once === true) {
+          this.events[eventName] = this.events[eventName].filter(
+            (elem) => elem.once !== true,
+          );
+        }
+      }
+    }
+  }
+  prependListener(eventName, listener) {
+    if (typeof listener !== 'function') {
+    } else {
+      if (!this.events[eventName]) {
+        this.events[eventName] = [];
+      }
+
+      this.events[eventName].unshift({ fn: listener, once: false });
+    }
+  }
+  prependOnceListener(eventName, listener) {
+    if (typeof listener !== 'function') {
+    } else {
+      if (!this.events[eventName]) {
+        this.events[eventName] = [];
+      }
+
+      this.events[eventName].unshift({ fn: listener, once: true });
+    }
+  }
+  removeAllListeners(eventName) {
+    if (eventName !== undefined) {
+      this.events[eventName] = [];
+    } else {
+      this.events = {};
+    }
+  }
+  listenerCount(eventName) {
+    if (!this.events[eventName]) {
+      return 0;
+    } else {
+      return this.events[eventName].filter(
+        (object) => typeof object.fn === 'function',
+      ).length;
+    }
+  }
 }
 
 module.exports = MyEventEmitter;
