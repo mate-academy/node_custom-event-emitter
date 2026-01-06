d6ab0a14cb9154950b597b17a757bded9157d946
Mariia Hula <maria.gula0307@gmail.com>
2026-01-06 16:48:50 +0200

    add validation and improve solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -2,19 +2,40 @@
 
 class MyEventEmitter {
   listeners = [];
-  nextId = 0;
+  #nextId = 0;
+
+  #validateEventName(eventName) {
+    if (typeof eventName !== 'string' && typeof eventName !== 'symbol') {
+      throw new TypeError('eventName must be a string or a symbol');
+    }
+  }
+
+  #validateListener(listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
+    }
+  }
 
   on(eventName, listener) {
-    this.listeners.push([eventName, listener, false, this.nextId++]);
+    this.#validateEventName(eventName);
+    this.#validateListener(listener);
+
+    this.listeners.push([eventName, listener, false, this.#nextId++]);
 
     return this;
   }
   once(eventName, listener) {
-    this.listeners.push([eventName, listener, true, this.nextId++]);
+    this.#validateEventName(eventName);
+    this.#validateListener(listener);
+
+    this.listeners.push([eventName, listener, true, this.#nextId++]);
 
     return this;
   }
   off(eventName, listener) {
+    this.#validateEventName(eventName);
+    this.#validateListener(listener);
+
     const index = this.listeners.findIndex(
       ([name, curListener]) => name === eventName && curListener === listener,
     );
@@ -26,6 +47,8 @@ class MyEventEmitter {
     return this;
   }
   emit(eventName, ...args) {
+    this.#validateEventName(eventName);
+
     const snapshot = this.listeners.slice();
     const onceListeners = [];
     let foundListener = false;
@@ -33,7 +56,10 @@ class MyEventEmitter {
     for (let i = 0; i < snapshot.length; i++) {
       const [name, listener, isOnce, id] = snapshot[i];
 
-      if (name === eventName) {
+      if (
+        name === eventName &&
+        Boolean(this.listeners.find(([, , , curId]) => curId === id))
+      ) {
         listener(...args);
         foundListener = true;
 
@@ -50,25 +76,33 @@ class MyEventEmitter {
     return foundListener;
   }
   prependListener(eventName, listener) {
-    this.listeners.unshift([eventName, listener, false, this.nextId++]);
+    this.#validateEventName(eventName);
+    this.#validateListener(listener);
+
+    this.listeners.unshift([eventName, listener, false, this.#nextId++]);
 
     return this;
   }
   prependOnceListener(eventName, listener) {
-    this.listeners.unshift([eventName, listener, true, this.nextId++]);
+    this.#validateEventName(eventName);
+    this.#validateListener(listener);
+
+    this.listeners.unshift([eventName, listener, true, this.#nextId++]);
 
     return this;
   }
   removeAllListeners(eventName) {
-    if (eventName) {
-      this.listeners = this.listeners.filter(([name]) => name !== eventName);
-    } else {
+    if (eventName === undefined) {
       this.listeners = [];
+    } else {
+      this.listeners = this.listeners.filter(([name]) => name !== eventName);
     }
 
     return this;
   }
   listenerCount(eventName) {
+    this.#validateEventName(eventName);
+
     return this.listeners.filter(([name]) => name === eventName).length;
   }
 }
