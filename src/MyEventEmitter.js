1bdec1720a9ed18c85c592d4af7dd9516c8223ec
Vasyl Pryimak <priymak17ukrvaha@gmail.com>
2026-02-18 15:26:58 +0200

    first commit

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,71 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  events = {};
+
+  on(eventName, handler) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].push(handler);
+  }
+
+  once(eventName, handler) {
+    const wrapper = (...args) => {
+      this.off(eventName, wrapper);
+      handler(...args);
+    };
+
+    this.on(eventName, wrapper);
+  }
+
+  off(eventName, callback) {
+    if (!this.events[eventName]) {
+      return;
+    }
+
+    this.events[eventName] = this.events[eventName].filter(
+      (cb) => cb !== callback,
+    );
+  }
+
+  emit(eventName, ...args) {
+    if (!this.events[eventName]) {
+      return;
+    }
+
+    this.events[eventName].forEach((cb) => cb(...args));
+  }
+
+  prependListener(eventName, handler) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].unshift(handler);
+  }
+
+  prependOnceListener(eventName, handler) {
+    const wrapper = (...args) => {
+      this.off(eventName, wrapper);
+      handler(...args);
+    };
+
+    this.prependListener(eventName, wrapper);
+  }
+
+  removeAllListeners(eventName) {
+    delete this.events[eventName];
+  }
+
+  listenerCount(eventName) {
+    if (this.events[eventName]) {
+      return this.events[eventName].length;
+    } else {
+      return 0;
+    }
+  }
 }
 
 module.exports = MyEventEmitter;
