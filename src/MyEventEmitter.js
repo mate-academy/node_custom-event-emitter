5f44b0569a15f263738f0c61077c0a73fa4d9169
nikolay <diedaemon@gmail.com>
2026-03-14 21:39:05 +0100

    task complete

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,97 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this.events = {};
+  }
+
+  checkEvent(event) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+  }
+
+  on(event, listener) {
+    this.checkEvent(event);
+
+    this.events[event].push({ listener });
+
+    return this;
+  }
+
+  once(event, listener) {
+    this.checkEvent(event);
+
+    this.events[event].push({ listener, once: true });
+
+    return this;
+  }
+
+  off(event, listener) {
+    this.events[event] = this.events[event].filter(
+      ({ listener: lis }) => lis !== listener,
+    );
+
+    if (!this.events[event].length) {
+      delete this.events[event];
+    }
+
+    return this;
+  }
+
+  emit(event, ...args) {
+    const listeners = this.events[event];
+
+    if (!listeners || !listeners.length) {
+      return false;
+    }
+
+    const copyListeners = [...this.events[event]];
+
+    copyListeners.forEach(({ listener, once }) => {
+      listener(...args);
+
+      if (once) {
+        this.off(event, listener);
+      }
+    });
+
+    return true;
+  }
+
+  prependListener(event, listener) {
+    this.checkEvent(event);
+    this.events[event].unshift({ listener });
+
+    return this;
+  }
+
+  prependOnceListener(event, listener) {
+    this.checkEvent(event);
+    this.events[event].unshift({ listener, once: true });
+
+    return this;
+  }
+
+  removeAllListeners(event) {
+    if (event) {
+      delete this.events[event];
+
+      return this;
+    }
+
+    this.events = {};
+
+    return this;
+  }
+
+  listenerCount(event) {
+    if (this.events[event]) {
+      return this.events[event].length;
+    }
+
+    return 0;
+  }
 }
 
 module.exports = MyEventEmitter;
