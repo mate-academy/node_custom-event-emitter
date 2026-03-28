d023301daf88cfdede4bb34934a1420bd0b185c5
adonch <donch.a080@gmail.com>
2026-03-28 20:43:36 +0200

    emmiter implementation

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,49 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this.listeners = [];
+  }
+  on(event, listener) {
+    this.listeners.push({ event, listener });
+  }
+  once(event, listener) {
+    const onceListener = (data) => {
+      listener(data);
+      this.off(event, onceListener);
+    };
+
+    this.on(event, onceListener);
+  }
+  off(event, listener) {
+    this.listeners = this.listeners.filter(
+      (l) => l.event !== event || l.listener !== listener,
+    );
+  }
+  emit(event, ...data) {
+    this.listeners.forEach((l) => {
+      if (l.event === event) {
+        l.listener(...data);
+      }
+    });
+  }
+  prependListener(event, listener) {
+    this.listeners.unshift({ event, listener });
+  }
+  prependOnceListener(event, listener) {
+    const onceListener = (data) => {
+      listener(data);
+      this.off(event, onceListener);
+    };
+
+    this.prependListener(event, onceListener);
+  }
+  removeAllListeners(event) {
+    this.listeners = this.listeners.filter((l) => l.event !== event);
+  }
+  listenerCount(event) {
+    return this.listeners.filter((l) => l.event === event).length;
+  }
 }
 
 module.exports = MyEventEmitter;
