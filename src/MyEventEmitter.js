74427099cbb4d665135d23361fb4b46314928a10
Artem Skichko <artem.skichko0@gmail.com>
2026-05-12 13:24:33 +0200

    Solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,91 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, listener) {
+    if (!listener) {
+      return 0;
+    }
+
+    if (!this.events[eventName]) {
+      this.events[eventName] = [{ callback: listener, once: false }];
+    } else {
+      this.events[eventName].push({ callback: listener, once: false });
+    }
+  }
+
+  once(eventName, listener) {
+    if (!listener) {
+      return 0;
+    }
+
+    if (!this.events[eventName]) {
+      this.events[eventName] = [{ callback: listener, once: true }];
+    } else {
+      this.events[eventName].push({ callback: listener, once: true });
+    }
+  }
+
+  off(eventName, listener) {
+    if (this.events[eventName]) {
+      this.events[eventName] = this.events[eventName].filter(
+        (l) => l.callback !== listener,
+      );
+    }
+  }
+
+  emit(eventName, ...args) {
+    if (!this.events[eventName]) {
+      return 0;
+    }
+
+    const listeners = [...this.events[eventName]];
+    let count = 0;
+
+    for (const listener of listeners) {
+      listener.callback(...args);
+
+      if (listener.once === true) {
+        this.events[eventName] = this.events[eventName].filter(
+          (ev) => ev !== listener,
+        );
+      }
+      count++;
+    }
+
+    return count;
+  }
+
+  prependListener(eventName, listener) {
+    if (!listener) {
+      return 0;
+    }
+
+    this.events[eventName].unshift({ callback: listener, once: false });
+  }
+  prependOnceListener(eventName, listener) {
+    if (!listener) {
+      return 0;
+    }
+
+    this.events[eventName].unshift({ callback: listener, once: true });
+  }
+  removeAllListeners(eventName) {
+    delete this.events[eventName];
+  }
+
+  listenerCount(eventName) {
+    if (!this.events[eventName]) {
+      return 0;
+    }
+
+    return this.events[eventName].length;
+  }
 }
 
 module.exports = MyEventEmitter;
