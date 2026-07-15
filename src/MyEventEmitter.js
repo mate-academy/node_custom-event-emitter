9cf3045d8f639cd7f11c3ec0c23175b1fa6d1c8e
Y. Kotvizky <ekotvizky@gmail.com>
2026-07-15 21:43:24 +0300

    v1

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,77 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  #prependEvents = {};
+  #events = {};
+
+  #addToCollection(events, eventName, eventHandler) {
+    if (!events[eventName]) {
+      events[eventName] = [];
+    }
+
+    events[eventName].push(eventHandler);
+  }
+
+  on(eventName, eventHandler) {
+    this.#addToCollection(this.#events, eventName, eventHandler);
+  }
+
+  once(eventName, eventHandler) {
+    this.#addToCollection(this.#events, eventName, [eventHandler]);
+  }
+
+  off(eventName, eventHandler) {
+    [this.#events, this.#prependEvents].forEach((events) => {
+      if (events[eventName]) {
+        const filtered = events[eventName].filter((handler) => {
+          if (Array.isArray(handler)) {
+            return eventHandler !== handler[0];
+          }
+
+          return handler !== eventHandler;
+        });
+
+        events[eventName] = filtered;
+      }
+    });
+  }
+
+  emit(eventName) {
+    [this.#prependEvents, this.#events].forEach((events) => {
+      if (events[eventName]) {
+        for (const handler of events[eventName]) {
+          if (Array.isArray(handler)) {
+            handler[0]();
+          } else {
+            handler();
+          }
+        }
+
+        events[eventName] = events[eventName].filter(
+          (handler) => !Array.isArray(handler),
+        );
+      }
+    });
+  }
+
+  prependListener(eventName, eventHandler) {
+    this.#addToCollection(this.#prependEvents, eventName, eventHandler);
+  }
+
+  prependOnceListener(eventName, eventHandler) {
+    this.#addToCollection(this.#prependEvents, eventName, [eventHandler]);
+  }
+
+  removeAllListeners() {
+    this.prependEvents = {};
+    this.events = {};
+  }
+
+  listenerCount(eventName) {
+    return [this.#prependEvents, this.#events].reduce((acc, events) => {
+      return acc + (events[eventName] ? events[eventName].length : 0);
+    }, 0);
+  }
 }
 
 module.exports = MyEventEmitter;
